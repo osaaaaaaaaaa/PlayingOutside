4c0017b72d625a4f7d08bbe5e85d2f6e79aaf0f5
S-JoBi23-053\student <231013@st.yoshida-g.ac.jp>
2024-11-11 09:25:35 +0900

    サーバー、クライアント、Shared、SharedOutputプロジェクト作成

---
+++ Server/Services/MyFirstService.cs
@@ -0,0 +1,18 @@
+﻿using MagicOnion;
+using MagicOnion.Server;
+using Shared.Interfaces.Services;
+
+public class MyFirstService : ServiceBase<IMyFirstService>, IMyFirstService
+{
+    public async UnaryResult<int> SumAsync(int x, int y)
+    {
+        Console.WriteLine("Received:" + x + "," + y);
+        return x + y;
+    }
+
+    public async UnaryResult<int> SubAsync(int x, int y)
+    {
+        Console.WriteLine("Received_Sub:" + x + "," + y);
+        return x - y;
+    }
+}
---
+++ Shared/Interfaces/Services/IMyFirstService.cs
@@ -0,0 +1,18 @@
+﻿using MagicOnion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Interfaces.Services
+{
+    public interface IMyFirstService : IService<IMyFirstService>
+    {
+        // ここのスペースに関数形式でどのような関数を作るのか書く
+
+        // 『足し算API』... 二つの整数を引数で受け取り合計を返す
+        UnaryResult<int> SumAsync(int x, int f);
+
+        // 『引算API』... 二つの整数を引数で受け取り合計を返す
+        UnaryResult<int> SubAsync(int x, int f);
+    }
+}
